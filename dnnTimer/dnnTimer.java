04d3793549847eed8dd9e6511357da8e5c5e6d1a
NitaiLavie <NitaiLavie@users.noreply.github.com>
2017-05-12 16:48:53 +0300

    created dnnTimer for timing measurements

---
+++ dnnTimer/dnnTimer.java
@@ -0,0 +1,35 @@
+package dnnUtil.dnnTimer;
+
+/**
+ * Created by nitai on 12/05/17.
+ */
+
+public class dnnTimer {
+    /**
+     * measures time passed between start and stop in milliseconds in milliseconds
+     * when start measuring sets time to 0
+     */
+    long mStartTime;
+    long mStopTime;
+    long mTime;
+
+    public dnnTimer(){
+        mStartTime = 0;
+        mStopTime = 0;
+        mTime = 0;
+    }
+
+    public void start(){
+        mTime = 0;
+        mStartTime = System.currentTimeMillis();
+    }
+
+    public void stop(){
+        mStopTime = System.currentTimeMillis();
+        mTime = mStopTime - mStartTime;
+    }
+
+    public long getTime(){
+        return mTime;
+    }
+}
