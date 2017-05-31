952bef8493f0fbc471323ecaee99e88dd74da418
TzoofHemed <TzoofHemed@users.noreply.github.com>
2017-05-31 10:20:34 +0300

    add dnnTrainingIndex

---
+++ dnnModel/DnnTrainingIndex.java
@@ -0,0 +1,48 @@
+package dnnUtil.dnnModel;
+
+public class DnnTrainingIndex {
+	private Integer mDataSize;		
+	private String mDataType;		//either TRAIN, VALIDATE OR TEST 
+	private Integer mDataIndex;
+	private String mDataSet;		//MNIST or CIFAR10
+	
+	public DnnTrainingIndex(Integer dataSize, String dataType, Integer dataIndex, String dataSet) {
+		setDataSize(dataSize);
+		setDataType(dataType);
+		setDataIndex(dataIndex);
+		setDataSet(dataSet);
+	}
+
+	public Integer getDataSize() {
+		return mDataSize;
+	}
+
+	public void setDataSize(Integer mDataSize) {
+		this.mDataSize = mDataSize;
+	}
+
+	public String getDataType() {
+		return mDataType;
+	}
+
+	public void setDataType(String mDataType) {
+		this.mDataType = mDataType;
+	}
+
+	public Integer getDataIndex() {
+		return mDataIndex;
+	}
+
+	public void setDataIndex(Integer mDataIndex) {
+		this.mDataIndex = mDataIndex;
+	}
+
+	public String getDataSet() {
+		return mDataSet;
+	}
+
+	public void setDataSet(String mDataSet) {
+		this.mDataSet = mDataSet;
+	}
+
+}
