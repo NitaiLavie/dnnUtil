01e0ce4bba0c89f2a7e1fde81dc9e87b243736fc
NitaiLavie <NitaiLavie@users.noreply.github.com>
2017-06-24 17:39:42 +0300

    added battery statistics to DnnStatistics

--- dnnStatistics/DnnStatistics.java
+++ dnnStatistics/DnnStatistics.java
@@ -5,27 +5,37 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class DnnStatistics implements Serializable {
-	static final long serialVersionUID = 4L;
+	static final long serialVersionUID = 5L;
 
 	private Map<String,String> mStats;
 	private String mClientName;
+	private String mTask;
 	private Long mStartTrainingTime = (long)0; // in milliseconds since epoch time
 	private Long mFinishTrainingTime = (long)0; // in milliseconds since epoch time
 	private String mDeviceName;
 	private String mMemoryFootprint;
 	private Integer mNumberOfTrainedEpochs = 0;
-	private String mSuccesRate;
 	private Integer mModelNumber = 0;
 	private Integer mServerInteractionCount = 0;
+	private Boolean mIsCharging = false;
+	private Float mStartBatteryLevel;
+	private Float mFinishBatteryLevel;
 	
 	public DnnStatistics(){
 		mStats = new LinkedHashMap<>();
 	}
+
 	public String getClientName() {
 		return mClientName;
 	}
 	public void setClientName(String mClientName) {
 		this.mClientName = mClientName;
+	}
+	public String getTask(){
+		return mTask;
+	}
+	public void setTask(String task){
+		this.mTask = task;
 	}
 	public Long getStartTrainingTime() {
 		return mStartTrainingTime;
@@ -57,12 +67,6 @@ public class DnnStatistics implements Serializable {
 	public void setNumberOfTrainedEpochs(Integer mNumberOfTrainedEpochs) {
 		this.mNumberOfTrainedEpochs = mNumberOfTrainedEpochs;
 	}
-	public String getSuccesRate() {
-		return mSuccesRate;
-	}
-	public void setSuccesRate(String mSuccesRate) {
-		this.mSuccesRate = mSuccesRate;
-	}
 	public Integer getModelNumber() {
 		return mModelNumber;
 	}
@@ -75,6 +79,25 @@ public class DnnStatistics implements Serializable {
 	public void setServerInteractionCount(int mServerInteractionCount) {
 		this.mServerInteractionCount = mServerInteractionCount;
 	}
+	public boolean getIsCharging(){
+		return mIsCharging;
+	}
+	public void setIsChargin(boolean isCharging){
+		this.mIsCharging = isCharging;
+	}
+	public float getStartBatteryLevel(){
+		return mStartBatteryLevel;
+	}
+	public void setStartBatteryLevel(float startBatteryLevel){
+		this.mStartBatteryLevel = startBatteryLevel;
+	}
+	public float getFinishBatteryLevel(){
+		return mFinishBatteryLevel;
+	}
+	public void setFinishBatteryLevel(float finishBatteryLevel){
+		this.mFinishBatteryLevel = finishBatteryLevel;
+	}
+	
 	public String getStatisticsCSVHeader(){
 		String csvHeader ="";
 		for (Map.Entry<String, String> statKey: mStats.entrySet()) {
@@ -116,14 +139,17 @@ public class DnnStatistics implements Serializable {
 	}
 	private void updateStatsMap(){
 		mStats.put("Client_Name", mClientName);
+		mStats.put("Task",mTask);
 		mStats.put("Device_Name", mDeviceName);
 		mStats.put("Model_Number", mModelNumber.toString());
 		mStats.put("Start_Training_Time", mStartTrainingTime.toString());
 		mStats.put("Finish_Training_Time", mFinishTrainingTime.toString());
 		mStats.put("Number_Of_Trained_Epochs",mNumberOfTrainedEpochs.toString());
 		mStats.put("Memory_Footprint",mMemoryFootprint);
-		mStats.put("Succes_Rate", mSuccesRate);
-		mStats.put("Server_Interaction_Count", new Integer(mServerInteractionCount).toString());		
+		mStats.put("Server_Interaction_Count", mServerInteractionCount.toString());
+		mStats.put("Is_Charging", mIsCharging.toString());
+		mStats.put("Start_Battery_Level", mStartBatteryLevel.toString());
+		mStats.put("Finish_Battery_Level", mFinishBatteryLevel.toString());
 		
 	}
 	
